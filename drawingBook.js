d647bed9e5d053401d2390c6e0eece651f986f28
AKDEAS\akdea <akdeaso@gmail.com>
2024-04-27 18:39:11 +0700

    add validation to func

--- drawingBook.js
+++ drawingBook.js
@@ -1,6 +1,10 @@
 export function pageCount(n, p) {
-  const fromFirst = Math.floor(p / 2);
-  const fromLast = Math.floor(n / 2 - fromFirst);
-  const minP = Math.min(fromFirst, fromLast);
-  return minP;
+  if (n < 1 || p < 1 || p > n) {
+    return 0;
+  } else {
+    const fromFirst = Math.floor(p / 2);
+    const fromLast = Math.floor(n / 2 - fromFirst);
+    const minP = Math.min(fromFirst, fromLast);
+    return minP;
+  }
 }
